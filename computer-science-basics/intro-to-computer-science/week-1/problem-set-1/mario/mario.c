5552f89297f139170a1702188f9b373ed6128cb3
Marc Rovira <marcrovirasacie@gmail.com>
2021-06-24 18:09:09 +0200

    [CSB1] Finished 1/2 problems of week 1

---
+++ computer-science-basics/intro-to-computer-science/week-1/problem-set-1/mario/mario.c
@@ -0,0 +1,39 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+    int height;
+
+    // Prompt for height of pyramid
+    do
+    {
+        height = get_int("Height (from 1 to 8): ");
+    }
+    while (height < 1 || height > 8);
+
+    // Print pyramid block characters
+    for (int i = 0; i < height; i++)
+    {
+        // Loop for left-hand side
+        for (int j = 0; j < height; j++)
+        {
+            char c = (j >= (height - 1) - i) ? '#' : ' ';
+            printf("%c", c);
+        }
+
+        printf("  ");
+
+        // Loop for right-hand side
+        for (int j = 0; j < height; j++)
+        {
+            if (j <= i)
+            {
+                printf("#");
+            }
+        }
+
+        printf("\n");
+    }
+}
