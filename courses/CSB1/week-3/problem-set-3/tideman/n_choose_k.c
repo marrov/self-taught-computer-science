6c14cb1500d1ca9e766bed9a3f9d4103fb367814
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-01 18:25:48 +0000

    [CSB1] Added function that computes binomial coefficients recursively

---
+++ courses/CSB1/week-3/problem-set-3/tideman/n_choose_k.c
@@ -0,0 +1,45 @@
+#include <cs50.h>
+#include <stdio.h>
+
+// Function prototypes
+int nck_factorial(int n, int k);
+long fact(int n);
+
+// Compute binomial coefficients in different manners
+int main(void)
+{
+    int n, k;
+
+    do
+    {
+        n = get_int("n: ");
+    }
+    while (n <= 0 || n > 15);
+
+    do
+    {
+        k = get_int("k: ");
+    }
+    while (k < 1 || k > n - 1);
+
+    printf("C(%i, %i) = %i\n", n, k, nck_factorial(n, k));
+}
+
+// Calculate binomial coefficients with recursion explicitly
+int nck_factorial(int n, int k)
+{
+    return (fact(n)) / (fact(k) * fact(n - k));
+}
+
+// Function to calculate the factorial of n (i.e. n!)
+long fact(int n)
+{
+    if (n == 1)
+    {
+        return 1;
+    }
+    else
+    {
+        return n*fact(n-1);
+    }
+}
