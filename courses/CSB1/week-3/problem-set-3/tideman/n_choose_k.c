d549b98fed4c49c7932785c61a18d1819fac5f67
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-05 18:11:08 +0000

    [CSB1] Added multiplicate version of binomial coefficients

--- courses/CSB1/week-3/problem-set-3/tideman/n_choose_k.c
+++ courses/CSB1/week-3/problem-set-3/tideman/n_choose_k.c
@@ -4,9 +4,11 @@
 // Function prototypes
 int nck_factorial(int n, int k);
 int nck_recursive(int n, int k);
+int nck_multiplicative(int n, int k);
 long fact(int n);
 
-// Compute binomial coefficients in different manners
+// Computes binomial coefficients in different manners
+// For theory see: https://en.wikipedia.org/wiki/Binomial_coefficient
 int main(void)
 {
     int n, k;
@@ -15,7 +17,7 @@ int main(void)
     {
         n = get_int("n: ");
     }
-    while (n <= 0 || n > 15);
+    while (n <= 0 || n > 20);
 
     do
     {
@@ -23,8 +25,22 @@ int main(void)
     }
     while (k < 1 || k > n - 1);
 
-    printf("Factorial: C(%i, %i) = %i\n", n, k, nck_factorial(n, k));
-    printf("Recursive: C(%i, %i) = %i\n", n, k, nck_recursive(n, k));
+    printf("Factorial:      C(%i, %i) = %i\n", n, k, nck_factorial(n, k));
+    printf("Recursive:      C(%i, %i) = %i\n", n, k, nck_recursive(n, k));
+    printf("Multiplicative: C(%i, %i) = %i\n", n, k, nck_multiplicative(n, k));
+}
+
+// Calculate binomial coefficients with the multiplicative formula
+int nck_multiplicative(int n, int k)
+{
+    float sum = 1;
+
+    for (int i = 1; i < k + 1; i++)
+    {
+        sum *= (n + 1. - i) / i;
+    }
+
+    return (int)sum;
 }
 
 // Calculate binomial coefficients with recursion explicitly using additive formula
