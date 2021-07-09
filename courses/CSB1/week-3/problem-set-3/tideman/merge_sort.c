3b92537c006df56f9d2f246696c645c7a5c53666
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-09 15:51:59 +0000

    [CSB1] merge_sort prints out left and right halves of arbitrary arrays

--- courses/CSB1/week-3/problem-set-3/tideman/merge_sort.c
+++ courses/CSB1/week-3/problem-set-3/tideman/merge_sort.c
@@ -3,11 +3,15 @@
 #include <string.h>
 
 // Global constants
+
 #define len 6
-int array[len] = {3, 4, 1, 5, 2, 6};
+int arr[len] = {5, 2, 1, 3, 6, 4};
+
+//#define len 2
+//int array[len] = {2, 1};
 
 // Function prototypes
-void merge_sort(void);
+void merge_sort(int fst, int lst);
 void print_array(void);
 
 int main(void)
@@ -15,24 +19,45 @@ int main(void)
 
     printf("before:\n"); print_array();
 
-    merge_sort();
+    merge_sort(0, len - 1);
 
     printf("\nafter:\n"); print_array();
 }
 
-void merge_sort(void)
+// Question: do I really need to pass the array???
+void merge_sort(int fst, int lst)
 {
-    // Pseudocode:
-    // 1) Sort left half
-    // 2) Sort right half
-    // 3) Merge halves
+    // Hint: define arrays with two indices, first (fst) and last (lst)
+
+    if (fst != lst)
+    {
+        // divide array in two halves
+
+        printf("\nleft:\n");
+        for (int i = fst; i <= ((lst + 1) / 2 ) - 1; i++)
+        {
+            printf("%i ", arr[i]);
+        }
+        printf("\n");
+
+        printf("\nright:\n");
+        for (int i = ((lst + 1) / 2 ); i <=  lst; i++)
+        {
+            printf("%i ", arr[i]);
+        }
+        printf("\n");
+    }
+    else
+    {
+        printf("%i", arr[0]);
+    }
 }
 
 void print_array(void)
 {
     for (int i = 0; i < len; i++)
     {
-        printf("%i ", array[i]);
+        printf("%i ", arr[i]);
     }
     printf("\n");
 }
