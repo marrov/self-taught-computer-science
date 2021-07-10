0cd066d0cf99c02645dadc7fe529791cfa7a3f6a
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-10 08:29:25 +0000

    [CSB1] merge_sort recusively prints out all halves of an array

--- courses/CSB1/week-3/problem-set-3/tideman/merge_sort.c
+++ courses/CSB1/week-3/problem-set-3/tideman/merge_sort.c
@@ -4,11 +4,11 @@
 
 // Global constants
 
-#define len 6
-int arr[len] = {5, 2, 1, 3, 6, 4};
+//#define len 6
+//int arr[len] = {5, 2, 1, 3, 6, 4};
 
-//#define len 2
-//int array[len] = {2, 1};
+#define len 5
+int arr[len] = {5, 2, 1, 3, 6};
 
 // Function prototypes
 void merge_sort(int fst, int lst);
@@ -17,40 +17,56 @@ void print_array(void);
 int main(void)
 {
 
-    printf("before:\n"); print_array();
+    printf("before:\n");
+    print_array();
 
     merge_sort(0, len - 1);
 
-    printf("\nafter:\n"); print_array();
+    printf("\nafter:\n");
+    print_array();
 }
 
 // Question: do I really need to pass the array???
 void merge_sort(int fst, int lst)
 {
-    // Hint: define arrays with two indices, first (fst) and last (lst)
+    int mid;
 
-    if (fst != lst)
+    // Check if array has only one element
+    if (fst == lst)
     {
-        // divide array in two halves
-
-        printf("\nleft:\n");
-        for (int i = fst; i <= ((lst + 1) / 2 ) - 1; i++)
-        {
-            printf("%i ", arr[i]);
-        }
-        printf("\n");
-
-        printf("\nright:\n");
-        for (int i = ((lst + 1) / 2 ); i <=  lst; i++)
-        {
-            printf("%i ", arr[i]);
-        }
-        printf("\n");
+        return;
+    }
+    else if ((lst - fst + 1) % 2 == 0)
+    {
+        // Calculate midpoint if array longer than one
+        // and has even number of elements
+        mid = (lst + fst + 1) / 2;
     }
     else
     {
-        printf("%i", arr[0]);
+        // Calculate midpoint if array longer than one
+        // and has odd number of elements
+        mid = (lst + fst) / 2;
     }
+
+    // divide array in two halves
+    printf("\nleft:\n");
+    for (int i = fst; i < mid; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+    merge_sort(fst, mid - 1);
+
+    printf("\nright:\n");
+    for (int i = mid; i < lst + 1; i++)
+    {
+        printf("%i ", arr[i]);
+    }
+    printf("\n");
+    merge_sort(mid, lst);
+
+    return;
 }
 
 void print_array(void)
