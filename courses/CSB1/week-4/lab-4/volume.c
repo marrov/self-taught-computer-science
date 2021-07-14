89d2c8e01d0804aa3211b4b359229bbd525de52a
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-14 16:05:40 +0000

    [CSB1] Started out week-4 lab-4 exercise

---
+++ courses/CSB1/week-4/lab-4/volume.c
@@ -0,0 +1,53 @@
+// Modifies the volume of an audio file
+
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+// Number of bytes in .wav header
+const int HEADER_SIZE = 44;
+
+int main(int argc, char *argv[])
+{
+    // Check command-line arguments
+    if (argc != 4)
+    {
+        printf("Usage: ./volume input.wav output.wav factor\n");
+        return 1;
+    }
+
+    // Open files and determine scaling factor
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    FILE *output = fopen(argv[2], "w");
+    if (output == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
+
+    // Get factor as float from string input
+    float factor = atof(argv[3]);
+
+    //Copy header from input file to output file
+
+    // Initialize header
+    uint8_t header[HEADER_SIZE];
+
+    // Reader header from input
+
+    // Write heeader to output
+
+
+    //Read samples from input file and write updated data to output file
+    int16_t buffer;
+
+    // Close files
+    fclose(input);
+    fclose(output);
+}
