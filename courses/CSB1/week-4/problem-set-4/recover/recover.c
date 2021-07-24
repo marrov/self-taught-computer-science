7f10c9c3bd3a2007670dbc742d5a6c94c341dc06
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-24 08:57:45 +0000

    [CSB1] Coded usage and input file opening

--- courses/CSB1/week-4/problem-set-4/recover/recover.c
+++ courses/CSB1/week-4/problem-set-4/recover/recover.c
@@ -3,5 +3,18 @@
 
 int main(int argc, char *argv[])
 {
+    // Check for invalid usage
+    if (argc != 2)
+    {
+        printf("Usage: recover input.raw\n");
+        return 1;
+    }
 
+    // Open input file
+    FILE *input = fopen(argv[1], "r");
+    if (input == NULL)
+    {
+        printf("Could not open file.\n");
+        return 1;
+    }
 }
