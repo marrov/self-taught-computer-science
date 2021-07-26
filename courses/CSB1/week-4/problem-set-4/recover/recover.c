b4c155c92235b0793b5e1223b3947e3ac54d9a79
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-26 11:29:58 +0000

    [CSB1] Recover identifies JPEG blocks and keeps track of the number of blocks between images

--- courses/CSB1/week-4/problem-set-4/recover/recover.c
+++ courses/CSB1/week-4/problem-set-4/recover/recover.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 typedef uint8_t BYTE;
 
-// Number of bytes in .wav header
+// Number of bytes in a memory block
 const int BLOCK_SIZE = 512;
+
+// Prototypes
+bool is_jepg(BYTE *buffer);
 
 int main(int argc, char *argv[])
 {
@@ -25,13 +29,31 @@ int main(int argc, char *argv[])
     }
 
     // Initialize buffer
-    BYTE buffer[BLOCK_SIZE]
+    BYTE buffer[BLOCK_SIZE];
+
+    // TEMPORARY
+    int count = 0;
 
     // Read to buffer until the end of card
     while (fread(buffer, sizeof(BYTE), BLOCK_SIZE, input) == BLOCK_SIZE)
     {
+        if (is_jepg(buffer))
+        {
+            printf("Number of blocks: %i\n", count);
+            printf("Jepg block!\n");
+            count = 0;
+        }
+        else
+        {
+            count++;
+        }
     }
 
     // Close files and free memory
     fclose(input);
+}
+
+bool is_jepg(BYTE *buffer)
+{
+    return (buffer[0] == 0xff && buffer[1] == 0xd8 && buffer[2] == 0xff && (buffer[3] & 0xf0) == 0xe0);
 }
