af80c8cf172faf4cbb9a64e5ceecc0964c343c7e
Marc Rovira <marcrovirasacie@gmail.com>
2021-07-28 15:13:29 +0000

    [CSB1] Almost finished load function and included two hash functions

--- courses/CSB1/week-5/problem-set-5/speller/dictionary.c
+++ courses/CSB1/week-5/problem-set-5/speller/dictionary.c
@@ -1,7 +1,10 @@
 // Implements a dictionary's functionality
 
-#include <stdbool.h>
 #include <ctype.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdbool.h>
 #include "dictionary.h"
 
 // Represents a node in a hash table
@@ -12,14 +15,15 @@ typedef struct node
 }
 node;
 
-// Number of buckets in hash table
-const unsigned int N = 26;
+// Number of buckets in hash table (N ~ sqrt(N_WORDS))
+const unsigned int N = 26; // 378
 
 // Hash table
+// NB: global variables are automatically initialized to zero (i.e NULL for pointers)
 node *table[N];
 
-// Number of words in dictionary
-const unsigned int n_words = 0;
+// (Global) number of words in dictionary
+int N_WORDS = 0;
 
 // Returns true if word is in dictionary, else false
 bool check(const char *word)
@@ -33,19 +37,74 @@ unsigned int hash(const char *word)
 {
     // First attempt: simple first letter in word hash function
     return toupper(word[0]) - 'A';
+
+    /*
+    // Second attemp: djb2 from http://www.cse.yorku.ca/~oz/hash.html
+    int c;
+    unsigned int hash = 5381;
+
+    while ((c = *word++))
+    {
+        hash = ((hash << 5) + hash) + c;
+    }
+
+    return hash % N;
+    */
 }
 
 // Loads dictionary into memory, returning true if successful, else false
 bool load(const char *dictionary)
 {
-    // TODO
+    // Open dictionary file
+    FILE *dictfile = fopen(dictionary, "r");
+    if (dictfile == NULL)
+    {
+        return false;
+    }
+
+    // Initialize temporary word
+    char tmpword[LENGTH + 1];
+
+    // Read strings from file one at a time
+    while (fscanf(dictfile, "%s", tmpword) != EOF)
+    {
+        // Allocate memory for new node
+        node *dictword = malloc(sizeof(node));
+        if (dictword == NULL)
+        {
+            return false;
+        }
+        else
+        {
+            // Copy word into node
+            strcpy(dictword->word, tmpword);
+            // Set next to NULL
+            dictword->next = NULL;
+        }
+
+        // Hash word to obtain hash value to use as index
+        unsigned int hashidx = hash(dictword->word);
+
+        // Insert node into hash table at hash index
+        dictword->next = table[hashidx];
+        table[hashidx] = dictword;
+
+        printf("%u \t %s \n", hashidx, table[hashidx]->word);
+
+        // Update count of words in dictionary
+        N_WORDS++;
+    }
+
+    // Close files and free memory
+    fclose(dictfile);
+
     return false;
 }
 
 // Returns number of words in dictionary if loaded, else 0 if not yet loaded
 unsigned int size(void)
 {
-    return n_words;
+    return N_WORDS;
 }
 
 // Unloads dictionary from memory, returning true if successful, else false
