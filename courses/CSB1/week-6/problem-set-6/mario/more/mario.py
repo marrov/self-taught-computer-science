9cf4b28cd9b8bfa9e6accdddd528ed5d3c7dd47f
Marc Rovira <marcrovirasacie@gmail.com>
2021-08-07 15:26:19 +0000

    [CSB1] Minor mod to hello and updated mario

--- courses/CSB1/week-6/problem-set-6/mario/more/mario.py
+++ courses/CSB1/week-6/problem-set-6/mario/more/mario.py
@@ -1,19 +1,20 @@
 from cs50 import get_int
 
-while True:
-  height = get_int("Height: ")
-  if height >= 1 and height <= 9:
-    break
-
-draw_pyramid(height)
-
-
 def draw_pyramid(h):
   """ Draws a mario pyramid of height h recursively."""
   # Base case
   if h == 1:
     return
-  
+
   # TODO: Recursive case
   return
 
+if __name__ == "__main__":
+    while True:
+        height = get_int("Height: ")
+        if height >= 1 and height <= 9:
+            break
+
+    print(f'You chose a height of {height}')
+
+    draw_pyramid(height)
