d00253b2c68c77ec9756cae3ad7502eaf2422d74
Marc Rovira <marcrovirasacie@gmail.com>
2021-08-08 11:06:53 +0000

    [CSB1] Mario.py creates right half of the pyramid recursively

--- courses/CSB1/week-6/problem-set-6/mario/more/mario.py
+++ courses/CSB1/week-6/problem-set-6/mario/more/mario.py
@@ -1,20 +1,20 @@
 from cs50 import get_int
 
 def draw_pyramid(h):
-  """ Draws a mario pyramid of height h recursively."""
-  # Base case
-  if h == 1:
-    return
-
-  # TODO: Recursive case
-  return
+    """Draws a mario pyramid of height h recursively."""
+    if h == 1: # Base case
+        print('#')
+        return
+    else: # Recursive case
+        draw_pyramid(h - 1)
+        print('#' * h)
+        return
 
 if __name__ == "__main__":
     while True:
         height = get_int("Height: ")
-        if height >= 1 and height <= 9:
+        if height >= 1 and height <= 8:
             break
 
     print(f'You chose a height of {height}')
-
     draw_pyramid(height)
