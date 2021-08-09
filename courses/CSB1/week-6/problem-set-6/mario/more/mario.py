71ea5ffb199516232c41ccce507248a490ea4b9b
Marc Rovira <marcrovirasacie@gmail.com>
2021-08-09 09:02:36 +0000

    [CSB1] Mario.py style improved

--- courses/CSB1/week-6/problem-set-6/mario/more/mario.py
+++ courses/CSB1/week-6/problem-set-6/mario/more/mario.py
@@ -1,16 +1,21 @@
 from cs50 import get_int
 
+
 def draw_pyramid(h, htotal):
     """Draws a mario pyramid of height h recursively."""
-    if h == 1: # Base case
+    if h == 1:
+        # Base case
         print(' ' * (htotal - h) + '#' + '  ' + '#')
         return
-    else: # Recursive case
+    else:
+        # Recursive case
         draw_pyramid(h - 1, htotal)
         print(' ' * (htotal - h) + '#' * h + '  ' + '#' * h)
         return
 
+
 if __name__ == "__main__":
+    # Ask user for int from 1 to 8
     while True:
         height = get_int("Height: ")
         if height >= 1 and height <= 8:
