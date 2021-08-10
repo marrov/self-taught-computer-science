968d93de8cc019bb16fd3fa4bbb114de73003bf2
Marc Rovira <54272586+marrov@users.noreply.github.com>
2021-08-10 23:33:32 +0200

    Defined template for credit

---
+++ courses/CSB1/week-6/problem-set-6/credit/credit.py
@@ -0,0 +1,10 @@
+import re
+
+def main():
+    # TODO: Use re to find AMEX, MASTERCARD, VISA or invalid
+    
+    # TODO: if not invalid, use Lunh's algorithm to check if valid
+    return
+
+if __name__ == "__main__":
+    main()
