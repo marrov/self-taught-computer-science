a9caff2e8340ab9cd6d1b371e1ee723dbcdc87f5
Marc Rovira <marcrovirasacie@gmail.com>
2021-08-11 20:52:08 +0000

    [CSB1] Updated credit

---
+++ courses/CSB1/week-6/problem-set-6/credit/credit.py
@@ -0,0 +1,25 @@
+import re
+
+def main():
+    # TODO: ask user for input
+    num = get_int("Number: ")
+
+    # TODO: Use re to find AMEX, MASTERCARD, VISA or invalid
+    if num == 1:
+        # AMEX
+    elif num == 2:
+        # MASTERCARD
+    elif num == 3:
+        # VISA
+    else:
+        # invalid
+
+    # TODO: if not invalid, use Lunh's algorithm to check if valid
+
+    return
+
+def luhn(num)
+    return true
+
+if __name__ == "__main__":
+    main()
