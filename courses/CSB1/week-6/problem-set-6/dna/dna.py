fef70c9d9697e698d05f200b0c9c2763ab3d80ca
Marc Rovira <marcrovirasacie@gmail.com>
2021-08-20 10:03:25 +0000

    [CSB1] Added template for dna.py

--- courses/CSB1/week-6/problem-set-6/dna/dna.py
+++ courses/CSB1/week-6/problem-set-6/dna/dna.py
@@ -1,1 +1,24 @@
+import csv
+import sys
 
+
+def main():
+    # TODO: Check for correct usage
+
+    # TODO: Open the CSV file and read into memory
+
+    # TODO: Open the DNA sequence and read into memory.
+
+    # TODO: For each of the STRs (from CSV file), compute the longest run of
+    #       consecutive repeats of the STR in the DNA sequence to identify.
+
+    # TODO: If the STR counts match with any individuals in the CSV file, your
+    #       program should print out the name of the matching individual.
+
+
+def helper():
+    return
+
+
+if __name__ == "__main__":
+    main()
