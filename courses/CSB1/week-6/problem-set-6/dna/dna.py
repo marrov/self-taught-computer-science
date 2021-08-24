6786dd1d348bf4bca62cef67a4cdadd296248fb5
Marc Rovira <marcrovirasacie@gmail.com>
2021-08-24 06:14:42 +0000

    [CSB1] Improved style of comments in dna

--- courses/CSB1/week-6/problem-set-6/dna/dna.py
+++ courses/CSB1/week-6/problem-set-6/dna/dna.py
@@ -10,7 +10,7 @@ def main():
     if len(sys.argv) != 3:
         sys.exit("Usage: python dna.py data.csv sequence.txt")
 
-    # Open the CSV file and read into memory
+    # Open the CSV file and read into memory as dictionary
     database = []
     with open(sys.argv[1], "r") as file:
         reader = csv.DictReader(file)
@@ -19,13 +19,13 @@ def main():
 
     keys = list(database[10)
 
-    # TODO: Open the DNA sequence and read into memory.
+    # Open the DNA sequence and read into memory
 
     # TODO: For each of the STRs (from CSV file), compute the longest run of
-    #       consecutive repeats of the STR in the DNA sequence to identify.
+    #       consecutive repeats of the STR in the DNA sequence to identify
 
     # TODO: If the STR counts match with any individuals in the CSV file, your
-    #       program should print out the name of the matching individual.
+    #       program should print out the name of the matching individual
 
 
 def helper():
