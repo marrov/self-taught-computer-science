6c00dfe8778f43d5c6f71ccf21c7935c05e3ccc3
Marc Rovira <marcrovirasacie@gmail.com>
2021-09-02 07:28:51 +0000

    [CSB1] SQL query 11 finished for movies

---
+++ courses/CSB1/week-7/problem-set-7/movies/11.sql
@@ -0,0 +1,1 @@
+SELECT title FROM (movies INNER JOIN ratings ON movie_id = id) WHERE id IN (SELECT movie_id FROM stars WHERE person_id = (SELECT id FROM people WHERE name = "Chadwick Boseman")) ORDER BY rating DESC LIMIT 5;
