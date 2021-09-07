33eebef8e493822f7f57ac100f16c190a9928b36
Marc Rovira <marcrovirasacie@gmail.com>
2021-09-07 06:57:53 +0000

    [CSB1] Added fourth query to log for fiftyville

--- courses/CSB1/week-7/problem-set-7/fiftyville/log.sql
+++ courses/CSB1/week-7/problem-set-7/fiftyville/log.sql
@@ -4,3 +4,5 @@ SELECT * FROM crime_scene_reports WHERE street="Chamberlin Street" AND descripti
 SELECT id, name, transcript FROM interviews WHERE transcript LIKE "%courthouse%" AND month = 7 AND day = 28;
 -- Following Ruth's interview, check the courthouse parking security info for license plates from 10:15 to 10:25
 SELECT * FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
+-- Draw up initial list of suspects based on license plate information from Ruth's interview
+SELECT name FROM people WHERE license_plate IN (SELECT * FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25);
