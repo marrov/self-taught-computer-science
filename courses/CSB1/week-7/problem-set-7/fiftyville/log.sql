77f47a8a90a4277a7f14f67d74830b304eb4ed61
Marc Rovira <marcrovirasacie@gmail.com>
2021-09-07 06:46:22 +0000

    [CSB1] Improved second query in log for fiftyville

--- courses/CSB1/week-7/problem-set-7/fiftyville/log.sql
+++ courses/CSB1/week-7/problem-set-7/fiftyville/log.sql
@@ -1,4 +1,4 @@
 -- Keep a log of any SQL queries you execute as you solve the mystery.
 SELECT * FROM crime_scene_reports WHERE street="Chamberlin Street" AND description LIKE "%duck%";
 -- Let's find the three interviews that mention the courthouse on the day of the crime
-SELECT * FROM interviews WHERE transcript LIKE "%courthouse%" AND month = 7 AND day = 28;
+SELECT id, name, transcript FROM interviews WHERE transcript LIKE "%courthouse%" AND month = 7 AND day = 28;
