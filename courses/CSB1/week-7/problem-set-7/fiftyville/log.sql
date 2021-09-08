6c0ffda5452942235b8afbdac49d20880f431887
Marc Rovira <marcrovirasacie@gmail.com>
2021-09-08 17:38:35 +0000

    [CSB1] Added sixth query to log.sql in fiftyville

--- courses/CSB1/week-7/problem-set-7/fiftyville/log.sql
+++ courses/CSB1/week-7/problem-set-7/fiftyville/log.sql
@@ -5,6 +5,8 @@ SELECT id, name, transcript FROM interviews WHERE transcript LIKE "%courthouse%"
 -- Following Ruth's interview, check the courthouse parking security info for license plates from 10:15 to 10:25
 SELECT * FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25;
 -- Draw up initial list of suspects based on license plate information from Ruth's interview
-SELECT name FROM people WHERE license_plate IN (SELECT * FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25);
+SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25);
 -- Draw up list of suspects based on ATM information from Eugene's interview
 SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND atm_location = "Fifer Street" AND transaction_type = "withdraw"));
+-- Narrow down the initial list using the second list of suspects
+SELECT name FROM people WHERE license_plate IN (SELECT license_plate FROM courthouse_security_logs WHERE month = 7 AND day = 28 AND hour = 10 AND minute >= 15 AND minute <= 25) INTERSECT SELECT name FROM people WHERE id IN (SELECT person_id FROM bank_accounts WHERE account_number IN (SELECT account_number FROM atm_transactions WHERE month = 7 AND day = 28 AND atm_location = "Fifer Street" AND transaction_type = "withdraw"));
