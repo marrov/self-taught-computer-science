4fbd565d6e16d448920fdd77ebc26aa59652a4a4
Marc Rovira <marcrovirasacie@gmail.com>
2021-10-26 07:22:34 +0000

    [CSB1] Added distribution code for week 9's lab.

---
+++ courses/CSB1/week-9/lab-9/application.py
@@ -0,0 +1,29 @@
+import os
+
+from cs50 import SQL
+from flask import Flask, flash, jsonify, redirect, render_template, request, session
+
+# Configure application
+app = Flask(__name__)
+
+# Ensure templates are auto-reloaded
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Configure CS50 Library to use SQLite database
+db = SQL("sqlite:///birthdays.db")
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+
+        # TODO: Add the user's entry into the database
+
+        return redirect("/")
+
+    else:
+
+        # TODO: Display the entries in the database on index.html
+
+        return render_template("index.html")
+
+
