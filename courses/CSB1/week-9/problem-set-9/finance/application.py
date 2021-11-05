5e4c125ada481d04a04b1ac44dfec7048d3b8871
Marc Rovira <marcrovirasacie@gmail.com>
2021-11-05 16:30:42 +0000

    [CSB1] Implemented q uote functionality!

--- courses/CSB1/week-9/problem-set-9/finance/application.py
+++ courses/CSB1/week-9/problem-set-9/finance/application.py
@@ -114,7 +114,17 @@ def logout():
 @login_required
 def quote():
     """Get stock quote."""
-    return apology("TODO")
+
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
+        quote_dict = lookup(request.form.get("symbol"))
+        if not quote_dict:
+            return apology("stock not found", 403)
+        else:
+            return render_template("quoted.html", quote_dict=quote_dict)
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("quote.html")
 
 
 @app.route("/register", methods=["GET", "POST"])
