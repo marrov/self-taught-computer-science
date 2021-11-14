c837fb5e6159787cc5a2a8f8957dffe4641bd353
Marc Rovira <marcrovirasacie@gmail.com>
2021-11-14 09:16:45 +0000

    [CSB1] Corrected error code returns in apologies

--- courses/CSB1/week-9/problem-set-9/finance/application.py
+++ courses/CSB1/week-9/problem-set-9/finance/application.py
@@ -47,7 +47,7 @@ if not os.environ.get("API_KEY"):
 @login_required
 def index():
     """Show portfolio of stocks"""
-    return apology("TODO")
+    return apology("TODO", 200)
 
 
 @app.route("/buy", methods=["GET", "POST"])
@@ -103,18 +103,18 @@ def login():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username exists and password is correct
         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-            return apology("invalid username and/or password", 403)
+            return apology("invalid username and/or password", 400)
 
         # Remember which user has logged in
         session["user_id"] = rows[0]["id"]
@@ -165,22 +165,22 @@ def register():
 
         # Ensure username was submitted
         if not request.form.get("username"):
-            return apology("must provide username", 403)
+            return apology("must provide username", 400)
 
         # Ensure password was submitted
         elif not request.form.get("password"):
-            return apology("must provide password", 403)
+            return apology("must provide password", 400)
 
         # Ensure password and re-typed password match
         elif not safe_str_cmp(request.form.get("password"), request.form.get("confirmation")):
-            return apology("password confirmation does not match", 403)
+            return apology("password confirmation does not match", 400)
 
         # Query database for username
         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
 
         # Ensure username does not exist
         if len(rows) != 0:
-            return apology(f"username {request.form.get('username')} already exists", 403)
+            return apology(f"username {request.form.get('username')} already exists", 400)
 
         # Register the user (i.e. add user and hash to the database)
         db.execute("INSERT INTO users (username, hash) VALUES(?, ?)", request.form.get("username"), generate_password_hash(request.form.get("password")))
