c8c6b230fff127e47325b3a640ec2c3c13516d65
Marc Rovira <marcrovirasacie@gmail.com>
2021-12-09 13:19:44 +0000

    [CSB1] Changed up folder structure

--- courses/CSB1/final-project/rebalance/application.py
+++ courses/CSB1/final-project/rebalance/application.py
@@ -38,11 +38,6 @@ Session(app)
 # Configure CS50 Library to use SQLite database
 db = SQL("sqlite:///rebalance.db")
 
-# Make sure API key is set
-if not os.environ.get("API_KEY"):
-    raise RuntimeError("API_KEY not set")
-
-
 @app.route("/")
 @login_required
 def index():
