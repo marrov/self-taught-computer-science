17ebd6421deea1122e578b459c3ab22f4b4b82a4
Marc Rovira <marcrovirasacie@gmail.com>
2021-12-15 14:09:24 +0100

    Added route for dashboard and removed login requirement from home

--- courses/CSB1/final-project/new-rebalance/rebalance/views.py
+++ courses/CSB1/final-project/new-rebalance/rebalance/views.py
@@ -6,8 +6,14 @@ from flask import Blueprint, render_template, request, flash, jsonify
 views = Blueprint('views', __name__)
 
 
-@views.route('/', methods=['GET', 'POST'])
-@login_required
+@views.route('/', methods=['GET'])
 def home():
-    """ Loads landing page"""
+    """Loads landing page"""
     return render_template("home.html", user=current_user)
+
+
+@views.route('/dashboard', methods=['GET', 'POST'])
+@login_required
+def dashboard():
+    """Logic for main dashboard"""
+    return render_template("dashboard.html", user=current_user)
