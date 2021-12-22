6bb96ec6a05fbda392cb217fdf89135977af01ac
Marc Rovira <marcrovirasacie@gmail.com>
2021-12-22 08:49:13 +0100

    Added many to many db for users/funds

--- courses/CSB1/final-project/rebalance/rebalance/__init__.py
+++ courses/CSB1/final-project/rebalance/rebalance/__init__.py
@@ -22,7 +22,7 @@ def create_app():
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
-    from .models import User
+    from .models import User, UserWithFund, Fund
 
     create_database(app)
 
--- courses/CSB1/final-project/rebalance/rebalance/models.py
+++ courses/CSB1/final-project/rebalance/rebalance/models.py
@@ -1,24 +1,29 @@
 from . import db
 from flask_login import UserMixin
-from sqlalchemy.sql import func
 
 MAX_LEN = 128
 
 
-class Funds(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    fund_name = db.Column(db.String(1000))
-    fund_isin = db.Column(db.String(100))
-    fund_allocation = db.Column(db.Integer)
+class UserWithFund(db.Model):
+    __tablename__ = 'userwithfund'
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), primary_key=True)
+    fund_id = db.Column(db.Integer, db.ForeignKey('fund.id'), primary_key=True)
+    allocation = db.Column(db.Integer)
+    fund = db.relationship("Fund", back_populates="users")
+    user = db.relationship("User", back_populates="funds")
 
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(MAX_LEN), unique=True)
     password = db.Column(db.String(MAX_LEN))
+    funds = db.relationship("UserWithFund", back_populates="user")
 
 
-class UserWithFund(db.Model):
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    fund_id = db.Column(db.Integer, db.ForeignKey('fund.id'))
-    
+class Fund(db.Model):
+    __tablename__ = 'fund'
+    id = db.Column(db.Integer, primary_key=True)
+    fund_name = db.Column(db.String(100))
+    fund_isin = db.Column(db.String(20), unique=True)
+    users = db.relationship("UserWithFund", back_populates="fund")
